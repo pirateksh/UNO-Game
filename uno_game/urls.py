06c61ce9435819ac804a5678595d11c283c71dd1
Kshitiz Srivastava <ksh1998@gmail.com>
2020-10-27 13:13:49 +0530

    Avatar Upload/Mime Check Implemented.

--- uno_game/urls.py
+++ uno_game/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib.auth import views
 
 
@@ -24,3 +26,6 @@ urlpatterns = [
     url(r'^reset/done/$', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
