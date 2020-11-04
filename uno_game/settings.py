4f61c980a4984753c1827efa50685ac63d6f77b9
Kshitiz Srivastava <ksh1998@gmail.com>
2020-11-04 16:44:49 +0530

    Preparing for production - fixed email issue

--- uno_game/settings.py
+++ uno_game/settings.py
@@ -193,8 +193,8 @@ EMAIL_HOST = config('EMAIL_HOST', default="")
 EMAIL_PORT = config('EMAIL_PORT', default="")
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default="")
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default="")
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', default="")
-EMAIL_USE_SSL = config('EMAIL_USE_SSL', default="")
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
 
 
 
