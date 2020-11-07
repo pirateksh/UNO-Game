86bea002f3a7552870cd988e937270656bf38349
Kshitiz Srivastava <ksh1998@gmail.com>
2020-11-07 19:28:37 +0530

    Preparing for production - debugging

--- uno_game/asgi.py
+++ uno_game/asgi.py
@@ -1,10 +1,11 @@
-
 import os
+import django
 
 # from django.core.asgi import get_asgi_application
 from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uno_game.settings')
 
+django.setup()
 # application = get_asgi_application()
 application = get_default_application()
