4e58f3244a585456bf6fa602fab7a001c672596a
Ankit Sangwan <ankit.sangwan1999@gmail.com>
2021-02-25 20:07:22 +0530

    Final wsgi.py

--- uno_game/wsgi.py
+++ uno_game/wsgi.py
@@ -7,6 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+
+# Original and Working:-
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +16,14 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uno_game.settings')
 
 application = get_wsgi_application()
+
+
+# I ran collectstatic cmd using below version and it went well, so if u r facing issue with collectstatic consider below version once.
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uno_game.settings')
+
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
