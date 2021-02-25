a5b6e6d64facece348cfb8c14eecbe99cc4fd4ca
Ankit Sangwan <ankit.sangwan1999@gmail.com>
2021-02-25 19:41:57 +0530

    Checking settings.py

--- uno_game/wsgi.py
+++ uno_game/wsgi.py
@@ -7,18 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'uno_game.settings')
-
-# application = get_wsgi_application()
-# application = DjangoWhiteNoise(application)
-
-
-# EARLIER:-
 import os
 
 from django.core.wsgi import get_wsgi_application
