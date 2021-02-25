a9ab1b197295ce62ab279c995a3c89d95a545bf9
Ankit Sangwan <ankit.sangwan1999@gmail.com>
2021-02-25 18:43:37 +0530

    Edited settings.py

--- uno_game/settings.py
+++ uno_game/settings.py
@@ -152,7 +152,11 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+# DONOT USE BELOW AS IT GIVES SERVER ERROR 500:-(Source: https://stackoverflow.com/a/56259827/11070092)
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # r = redis.from_url(os.environ.get("REDIS_URL", 'redis://localhost:6379'))
 CHANNEL_LAYERS = {
     "default": {
--- uno_game/urls.py
+++ uno_game/urls.py
@@ -29,6 +29,6 @@ urlpatterns = [
 
 # To make Django serve Media File irrespective to DEBUG value. 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 # if settings.DEBUG:
 #     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
